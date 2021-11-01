ece792d5136bbda2266f9e3a62cb42c6994bd38a
Hans Ole Gjerdrum <hog@eika.no>
2021-11-01 11:17:44 +0100

    [Optional]: removed braces and replaced Nothing with Optional<T>

--- src/maybe.ts
+++ src/maybe.ts
@@ -1,96 +1,81 @@
-type Nothing = null | undefined;
+export type Optional<T> = T | undefined;
 
 export class Maybe<Value> {
-  private readonly wrappedValue: Value | Nothing;
+  private readonly wrappedValue: Optional<Value>;
 
-  constructor(wrappedValue: Value | Nothing) {
+  constructor(wrappedValue: Optional<Value>) {
     this.wrappedValue = wrappedValue;
   }
 
   // Extracting the value
+  valueOr = <T>(defaultValue: T): Value | T =>
+    Maybe.isSomething(this.wrappedValue) ? this.wrappedValue : defaultValue;
 
-  valueOr = (defaultValue: Value): Value => {
-    return Maybe.isSomething(this.wrappedValue) ? this.wrappedValue : defaultValue;
-  };
-
-  valueOrThrow = (error: Error = new TypeError('Wrapped value is undefined or null')): Value | never => {
-    return this.valueOrExecute(() => {
+  valueOrThrow = (
+    error: Error = new TypeError("Wrapped value is undefined or null"),
+  ): Value | never =>
+    this.valueOrExecute(() => {
       throw error;
     });
-  };
 
-  valueOrExecute = (closure: () => Value): Value => {
-    return Maybe.isSomething(this.wrappedValue) ? this.wrappedValue : closure();
-  };
+  valueOrExecute = (closure: () => Value): Value =>
+    Maybe.isSomething(this.wrappedValue) ? this.wrappedValue : closure();
 
   // Existence checking
-
   isSomething = () => {
     return !this.isNothing();
   };
 
-  isNothing = () => {
-    return this.wrappedValue === undefined || this.wrappedValue === null;
-  };
+  isNothing = () =>
+    this.wrappedValue === undefined || this.wrappedValue === null;
 
   // Mapping
-
-  map = <Output>(mapper: (value: Value) => Output | Nothing): Maybe<Output> => {
-    if (this.isNothing()) {
-      return Maybe.nothing();
-    }
+  map = <Output>(mapper: (value: Value) => Optional<Output>): Maybe<Output> =>
+    // eslint-disable-next-line
     // @ts-ignore
-    return new Maybe(mapper(this.wrappedValue));
-  };
+    this.isNothing() ? Maybe.nothing() : new Maybe(mapper(this.wrappedValue));
 
-  flatMap = <Output>(mapper: (value: Value) => Maybe<Output>): Maybe<Output> => {
-    return this.map(mapper).map(it => it.wrappedValue);
-  };
+  flatMap = <Output>(mapper: (value: Value) => Maybe<Output>): Maybe<Output> =>
+    this.map(mapper).map((it) => it.wrappedValue);
 
   // Conditional callbacks
-
   ifNothing = (callback: () => void): Maybe<Value> => {
     if (this.isNothing()) {
       callback();
     }
-
     return this;
   };
 
-  ifSomething = (callback: (_: Value) => void): Maybe<Value> => {
-    return this.map(value => {
+  ifSomething = (callback: (_: Value) => void): Maybe<Value> =>
+    this.map((value) => {
       callback(value);
       return value;
     });
-  };
 
   // Filtering
+  nothingUnless = (filter: (_: Value) => boolean): Maybe<Value> =>
+    this.map((value) => (filter(value) ? value : undefined));
 
-  nothingUnless = (filter: (_: Value) => boolean): Maybe<Value> => {
-    return this.map(value => (filter(value) ? value : undefined));
-  };
-
-  nothingIf = (filter: (_: Value) => boolean): Maybe<Value> => {
-    return this.nothingUnless(it => !filter(it));
-  };
+  nothingIf = (filter: (_: Value) => boolean): Maybe<Value> =>
+    this.nothingUnless((it) => !filter(it));
 
   // Utilities
-
   or = (other: Maybe<Value>): Maybe<Value> => (this.isNothing() ? other : this);
-
-  orJust = (other: Value): Maybe<Value> => (this.isNothing() ? Maybe.just(other) : this);
-
+  orJust = (other: Value): Maybe<Value> =>
+    this.isNothing() ? Maybe.just(other) : this;
   zip<OtherValue>(other: Maybe<OtherValue>): Maybe<[Value, OtherValue]> {
-    return this.map(it => other.map((t: OtherValue): [Value, OtherValue] => [it, t]).wrappedValue);
+    return this.map(
+      (it) =>
+        other.map((t: OtherValue): [Value, OtherValue] => [it, t]).wrappedValue,
+    );
   }
 
-  mapTo = <Key extends keyof Value>(key: Key): Maybe<Value[Key]> => this.map(value => value[key]);
-
-  stringify = (): Maybe<string> => this.map(value => JSON.stringify(value).replace(/\"/g, ""));
-
+  mapTo = <Key extends keyof Value>(key: Key): Maybe<Value[Key]> =>
+    this.map((value) => value[key]);
+  stringify = (): Maybe<string> =>
+    this.map((value) => JSON.stringify(value).replace(/\"/g, ""));
 
   // Static members
-
   static just<Value>(value: Value): Maybe<Value> {
     return new Maybe<Value>(value);
   }
@@ -99,16 +84,16 @@ export class Maybe<Value> {
     return new Maybe<Value>(undefined);
   }
 
-  static isSomething<Value>(value: Value | Nothing): value is Value {
+  static isSomething<Value>(value: Optional<Value>): value is Value {
     return !this.isNothing(value);
   }
 
-  static isNothing<Value>(value: Value | Nothing): value is Nothing {
+  static isNothing<Value>(value: Optional<Value>): value is undefined {
     return new Maybe(value).isNothing();
   }
 }
 
-export default function maybe<Value>(value: Value | Nothing): Maybe<Value> {
+export default function maybe<Value>(value: Optional<Value>): Maybe<Value> {
   if (Maybe.isNothing(value)) {
     return Maybe.nothing();
   }
