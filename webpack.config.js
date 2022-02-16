0b1e17f50fe78eaa8ff4b629c75e06fba4f3ec0e
Hans Ole Gjerdrum <hansogj@gmail.com>
2022-02-16 20:09:20 +0100

    [build]: global-this

--- webpack.config.js
+++ webpack.config.js
@@ -5,44 +5,45 @@ const path = require("path");
 const isProduction = process.env.NODE_ENV == "production";
 
 const config = {
-  entry: "./src/maybe.ts",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: 'maybe.js',
-    libraryTarget: 'umd',
-    library: 'maybe'
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
-  },
-  devtool: "source-map",
-  plugins: [ ],
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/i,
-        loader: "babel-loader",
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: "asset",
-      },
-
-      {
-        test: /\.(ts|tsx)$/i,
-        loader: 'ts-loader',
-        exclude: ['/node_modules/'],
+    entry: "./src/maybe.ts",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: 'maybe.js',
+        libraryTarget: 'umd',
+        globalObject: "this",
+        library: 'maybe'
     },
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    },
+    devtool: "source-map",
+    plugins: [],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/i,
+                loader: "babel-loader",
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+                type: "asset",
+            },
 
-    ],
-  },
+            {
+                test: /\.(ts|tsx)$/i,
+                loader: 'ts-loader',
+                exclude: ['/node_modules/'],
+            },
+
+        ],
+    },
 };
 
 module.exports = () => {
-if (isProduction) {
-    config.mode = "production";
-  } else {
-    config.mode = "development";
-} 
-  return config;
+    if (isProduction) {
+        config.mode = "production";
+    } else {
+        config.mode = "development";
+    }
+    return config;
 };
